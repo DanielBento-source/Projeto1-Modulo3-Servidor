560645219afee5c69e8f8f5a265c985aec6a0679
DanielBento <dsbld2@gmail.com>
2022-04-27 16:21:52 -0300

    feat: alterando conexão para db em cloud

--- src/database/database.js
+++ src/database/database.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
   mongoose
-    .connect('mongodb://127.0.0.1:27017/paletas-db', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      'mongodb+srv://root:admin@api-elgeladon.jai1f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    )
     .then(() => console.log('MONGODB CONNECT!'))
     .catch((error) =>
       console.log(`Erro ao conectar com MONGODB, erro ${error}`),
